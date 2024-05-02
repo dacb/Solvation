44de934699be87c473d16fe0c58ba4c75f521185
FaizaAB <121907697+FaizaAB@users.noreply.github.com>
2024-05-02 11:59:56 -0700

    adding mdm

---
+++ mdm/__init__.py
@@ -0,0 +1,5 @@
+from . import config
+from . import mdm_utils
+from . import mdm_model
+from . import train
+
---
+++ mdm/config.py
@@ -0,0 +1,6 @@
+data_dir = "/home/fostooq/solubility-prediction-paper/data/"
+best_model = "mdm/mdm_best.h5"
+max_epochs = 1000
+batch_size = 64
+patience=25
+lr = 10**-3
