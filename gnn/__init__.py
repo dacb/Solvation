85b8c7e70bcf66fc4e260d4f0f38b709d2513447
FaizaAB <121907697+FaizaAB@users.noreply.github.com>
2024-05-02 11:58:07 -0700

    Adding gnn folder

---
+++ gnn/__init__.py
@@ -0,0 +1,4 @@
+from . import config
+from . import gnn_utils
+from . import gnn_model
+from . import train
---
+++ gnn/config.py
@@ -0,0 +1,8 @@
+data_dir = "/home/fostooq/solubility-prediction-paper/data/"
+gnn_data_dir = "/home/fostooq/solubility-prediction-paper/data/"
+n_features = 65
+bs = 64
+max_epochs = 1000
+lr = 1e-3
+patience=25
+best_model = 'gnn/gnn_best.pt'
