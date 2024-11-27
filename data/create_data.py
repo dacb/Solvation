6de3666006d1fbf0a98d158b6b54bf02c11ee67f
Dave Beck <dacb@uw.edu>
2024-11-27 11:26:23 -0800

    added refactored code with main()

--- data/create_data.py
+++ data/create_data.py
@@ -81,96 +81,103 @@ def get_float_features(dft):
             pass
     return floats
 
-# location of the xyz coordinates
-xyz_dir = './pybel_xyz/with_H/'
+def main():
 
-# common fragments
-with open("./fragments.dat", "rb") as f:
-    fragments = pickle.load(f)
+    # location of the xyz coordinates
+    xyz_dir = './pybel_xyz/with_H/'
 
-# load the data downloaded from  
-# https://figshare.com/s/6258a546a27a2373bf2a/articles/14558808
-df = pd.read_csv("./data.csv", encoding='iso-8859-1')
+    # common fragments
+    with open("./fragments.dat", "rb") as f:
+        fragments = pickle.load(f)
 
-# convert smiles to canonical format
-df['rdkit_smiles'] = np.nan
-for i in tqdm(range(df.shape[0])):
-    try:
-        df.loc[i,'rdkit_smiles'] = Chem.MolToSmiles(Chem.MolFromSmiles(df.loc[i, 'SMILES']))
-    except RuntimeError as e:
-        print('caught a fatal runtime: ', e)
-        raise
-    except:
-        pass
+    # load the data downloaded from  
+    # https://figshare.com/s/6258a546a27a2373bf2a/articles/14558808
+    df = pd.read_csv("./data.csv", encoding='iso-8859-1')
+
+    # convert smiles to canonical format
+    df['rdkit_smiles'] = np.nan
+    for i in tqdm(range(df.shape[0])):
+        try:
+            df.loc[i,'rdkit_smiles'] = Chem.MolToSmiles(Chem.MolFromSmiles(df.loc[i, 'SMILES']))
+        except RuntimeError as e:
+            print('caught a fatal runtime: ', e)
+            raise
+        except:
+            pass
     
 
-df['log_sol'] = np.log10(1e-3*df['Experimental Solubility in Water']/df['Molar Mass'])
-df.reset_index(drop=True, inplace=True)
-dups = df[df.duplicated(subset=['rdkit_smiles'])]
+    df['log_sol'] = np.log10(1e-3*df['Experimental Solubility in Water']/df['Molar Mass'])
+    df.reset_index(drop=True, inplace=True)
+    dups = df[df.duplicated(subset=['rdkit_smiles'])]
 
-if dups.shape[0] !=0:
-    print("Warning: There are duplicate smiles!")
+    if dups.shape[0] !=0:
+        print("Warning: There are duplicate smiles!")
 
-else:    
-    # create pybel xyz data
-    create_xyz(df)
+    else:    
+        # create pybel xyz data
+        create_xyz(df)
 
-    # calculate mordred features
-    calc = Calculator(descriptors, ignore_3D=True)
-    mols = [Chem.MolFromSmiles(i) for i in df.rdkit_smiles.values]
-    df_3d = calc.pandas(mols)
-    features = df_3d.columns.tolist()
+        # calculate mordred features
+        calc = Calculator(descriptors, ignore_3D=True)
+        mols = [Chem.MolFromSmiles(i) for i in df.rdkit_smiles.values]
+        df_3d = calc.pandas(mols)
+        features = df_3d.columns.tolist()
 
-    # find features containing missing values
-    # such columns are removed
-    missing_features = find_missing(df_3d, features)      
-    newf = list(set(df_3d.columns.values).difference(list(set(missing_features))))
-    df_3d = df_3d.loc[:, newf]
+        # find features containing missing values
+        # such columns are removed
+        missing_features = find_missing(df_3d, features)      
+        newf = list(set(df_3d.columns.values).difference(list(set(missing_features))))
+        df_3d = df_3d.loc[:, newf]
     
-    # only use features that can be converted to floats
-    floats = get_float_features(df_3d)
-    df_3d = df_3d.loc[:, floats]
+        # only use features that can be converted to floats
+        floats = get_float_features(df_3d)
+        df_3d = df_3d.loc[:, floats]
     
-    # drop other nan columns
-    df_3d = df_3d.astype(float)
-    df_3d.dropna(axis=1 , inplace=True)
-    df_3d.reset_index(drop=True, inplace=True)
+        # drop other nan columns
+        df_3d = df_3d.astype(float)
+        df_3d.dropna(axis=1 , inplace=True)
+        df_3d.reset_index(drop=True, inplace=True)
 
-    # add the important data
-    df_3d.loc[:, 'smiles'] = df.rdkit_smiles
-    df_3d.loc[:, 'log_sol'] = df.log_sol
-    df_3d.loc[:, 'cas'] = df['CAS Number']
-    df_3d.loc[:, 'ref'] = df['Experiment Reference']
-    df_3d.loc[:, 'temp'] = df['Temperature']
-    df_3d.loc[:, 'inchi'] = df['Standard InChIKey']
+        # add the important data
+        df_3d.loc[:, 'smiles'] = df.rdkit_smiles
+        df_3d.loc[:, 'log_sol'] = df.log_sol
+        df_3d.loc[:, 'cas'] = df['CAS Number']
+        df_3d.loc[:, 'ref'] = df['Experiment Reference']
+        df_3d.loc[:, 'temp'] = df['Temperature']
+        df_3d.loc[:, 'inchi'] = df['Standard InChIKey']
 
-    df_3d = create_momentf(df_3d, xyz_dir, 'cas', fragments)
-
-
-    # check whether there are nan coordinates or xyz coordinates are unavailable
-    nans = check_for_nan_coords(df_3d, xyz_dir)
-    df_3d = df_3d[~df_3d.cas.isin(nans)]
-    df_3d.reset_index(drop=True, inplace=True)
+        df_3d = create_momentf(df_3d, xyz_dir, 'cas', fragments)
 
 
-    # split the data into train/valid/test sets
-    df_3d.loc[:,"sol_id"] = np.nan
-    df_3d.loc[df_3d.log_sol<=-8,"sol_id"] = 0
-    df_3d.loc[(df_3d.log_sol>-8) & (df_3d.log_sol<=-6),"sol_id"] = 1
-    df_3d.loc[(df_3d.log_sol>-6) & (df_3d.log_sol<=-4),"sol_id"] = 2
-    df_3d.loc[(df_3d.log_sol>-4) & (df_3d.log_sol<=-2),"sol_id"] = 3
-    df_3d.loc[(df_3d.log_sol>-2) & (df_3d.log_sol<=0),"sol_id"] = 4
-    df_3d.loc[df_3d.log_sol>0 ,"sol_id"] = 5
+        # check whether there are nan coordinates or xyz coordinates are unavailable
+        nans = check_for_nan_coords(df_3d, xyz_dir)
+        df_3d = df_3d[~df_3d.cas.isin(nans)]
+        df_3d.reset_index(drop=True, inplace=True)
 
-    train, test = train_test_split(df_3d, stratify = df_3d.sol_id.values, test_size=0.15)
-    train, val = train_test_split(train, stratify = train.sol_id.values, test_size=0.2)
 
-    train.drop(['sol_id'], axis=1, inplace=True)
-    test.drop(['sol_id'], axis=1, inplace=True)
-    val.drop(['sol_id'], axis=1, inplace=True)
+        # split the data into train/valid/test sets
+        df_3d.loc[:,"sol_id"] = np.nan
+        df_3d.loc[df_3d.log_sol<=-8,"sol_id"] = 0
+        df_3d.loc[(df_3d.log_sol>-8) & (df_3d.log_sol<=-6),"sol_id"] = 1
+        df_3d.loc[(df_3d.log_sol>-6) & (df_3d.log_sol<=-4),"sol_id"] = 2
+        df_3d.loc[(df_3d.log_sol>-4) & (df_3d.log_sol<=-2),"sol_id"] = 3
+        df_3d.loc[(df_3d.log_sol>-2) & (df_3d.log_sol<=0),"sol_id"] = 4
+        df_3d.loc[df_3d.log_sol>0 ,"sol_id"] = 5
 
-    train.to_csv("./train.csv", index=False)
-    test.to_csv("./test.csv", index=False)
-    val.to_csv("./val.csv", index=False)
+        train, test = train_test_split(df_3d, stratify = df_3d.sol_id.values, test_size=0.15)
+        train, val = train_test_split(train, stratify = train.sol_id.values, test_size=0.2)
 
-    print(f"data creation completed at {datetime.datetime.now()}")
+        train.drop(['sol_id'], axis=1, inplace=True)
+        test.drop(['sol_id'], axis=1, inplace=True)
+        val.drop(['sol_id'], axis=1, inplace=True)
+
+        train.to_csv("./train.csv", index=False)
+        test.to_csv("./test.csv", index=False)
+        val.to_csv("./val.csv", index=False)
+
+        print(f"data creation completed at {datetime.datetime.now()}")
+
+    return
+
+if __name__ == "__main__":
+    main()
